077cad89fad59b345f791210295356e3348c71ee
viictorcamposs <campos.viictor@gmail.com>
2021-01-19 17:06:22 -0300

    setting up routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
+const multer = require("multer");
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
+const BaseController = require('./controllers/BaseController');
+
+routes.get('/', BaseController.index);
+routes.post('/', multer(multerConfig).single('file'), BaseController.post);
+routes.delete('/:id', BaseController.delete);
 
 module.exports = routes; 
